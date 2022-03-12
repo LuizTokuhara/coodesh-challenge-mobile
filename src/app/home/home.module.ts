69de5fec2745242d2e97d34df1159fb9e359a1ad
Luiz Tokuhara <tokuharaluiz@gmail.com>
2022-03-12 12:28:21 -0300

    Add user card component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { UserCardComponent } from '../components/user-card/user-card.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, UserCardComponent],
 })
 export class HomePageModule {}
